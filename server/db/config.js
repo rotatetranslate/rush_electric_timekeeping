f3d74c822b4e3ba886ae94ed06ae163c6d5913ab
rotatetranslate <adamberro@gmail.com>
2018-05-09 07:37:03 -0700

    update db config

--- server/db/config.js
+++ server/db/config.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
-// const url = process.env.MONGODB_URI || 'mongodb://localhost/rush_electric'
-const url = 'mongodb://localhost/rush_electric'
+const url = process.env.MONGODB_URI || 'mongodb://localhost/rush_electric'
 
 mongoose.connect(url)
 
