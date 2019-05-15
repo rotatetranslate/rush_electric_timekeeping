4a8a1c1d5195fb891c61e9cf1b60049551ddcf8a
rotatetranslate <adamberro@gmail.com>
2019-05-14 20:33:03 -0700

    email

--- server/routes/mail.js
+++ server/routes/mail.js
@@ -2,8 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { mailForm } = require('../helpers')
 
-// const rushElectricEmail = 'rushelectric@att.net'
-const rushElectricEmail = 'adamberro@gmail.com'
+const rushElectricEmail = 'rushelectric@att.net'
 
 router.post('/', async (req, res, next) => {
   const {
